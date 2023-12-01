962a408a76431486928ac455f64df07c9492a5c0
briiiiiiiiiiiiii <152580697+briiiiiiiiiiiiii@users.noreply.github.com>
2023-12-01 21:06:01 +0800

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yayyy, Skyranch tayo :)))";
+  question.innerHTML = "Yayyy, Skyranch tayo, whenever you're free :)))";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
